76e7fb8db767d15319ac3ce4b924d6cd84b5e614
Stgo Larrain <stgo.larrain@gmail.com>
2014-06-17 12:29:58 -0400

    Less false-positive for face detector

--- src/face_detector.py
+++ src/face_detector.py
@@ -7,10 +7,16 @@ class FaceDetector(object):
 
 	def detect(self, img):
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		faces = self.face_cascade.detectMultiScale(gray,1.1,1)
+		faces = self.face_cascade.detectMultiScale(gray,1.5,3)
+		delta = 5
+		max_h, max_w = gray.shape
 		detections = []
 		for (x,y,w,h) in faces:
-			roi_gray = gray[y:y+h, x:x+w]
+			y1 = max(0, y-delta)
+			y2 = min(y+delta+h, max_h)
+			x1 = max(0, x-delta)
+			x2 = min(x+delta+w, max_w)
+			roi_gray = gray[y1:y2, x1:x2]
 			roi_gray = cv2.resize(roi_gray, (112,92))
 			detections.append(roi_gray)
 
@@ -19,7 +25,7 @@ class FaceDetector(object):
 if __name__ == '__main__':
 	import pickle
 	
-	images = pickle.load(open('pickles2.p', 'r'))
+	images = pickle.load(open('pickles2_2.p', 'r'))
 	face_detector = FaceDetector()
 	
 	for img in images:
@@ -29,8 +35,8 @@ if __name__ == '__main__':
 				cv2.imshow('Detection', f)
 				k = cv2.waitKey(0)
 
-			#cv2.imshow('Image', img)
-			#k = cv2.waitKey(0)
+			cv2.imshow('Image', img)
+			k = cv2.waitKey(0)
 
 			if k==27:
 				break
